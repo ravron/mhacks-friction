81a04b008559985d5d935a081003a07d51000934
Vanimal <vwitteki@purdue.edu>
2013-09-21 12:57:00 -0400

    Changes to plot

--- mhacks-friction/mhacks-friction/RAMFGraphViewController.m
+++ mhacks-friction/mhacks-friction/RAMFGraphViewController.m
@@ -29,9 +29,7 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    
-    NSLog(@"View X, Y, W, H: %f %f %f %f", self.hostView.frame.origin.x, self.hostView.frame.origin.y,
-          self.hostView.frame.size.width, self.hostView.frame.size.height);
+
     // Create a CPTGraph object and add to hostView
     CPTGraph* graph = [[CPTXYGraph alloc] initWithFrame:self.hostView.bounds];
     
@@ -43,30 +41,27 @@
     
     x.labelingPolicy = CPTAxisLabelingPolicyAutomatic;
     y.labelingPolicy = CPTAxisLabelingPolicyAutomatic;
-    
-    //x.labelAlignment = -.1f;
-    //y.labelAlignment = +.1f;
+
     
     
     // Get the (default) plotspace from the graph so we can set its x/y ranges
     CPTXYPlotSpace *plotSpace = (CPTXYPlotSpace *) graph.defaultPlotSpace;
     
     // Note that these CPTPlotRange are defined by START and LENGTH (not START and END) !!
-    [plotSpace setYRange: [CPTPlotRange plotRangeWithLocation:CPTDecimalFromFloat( -10 ) length:CPTDecimalFromFloat( 20 )]];
+    [plotSpace setYRange: [CPTPlotRange plotRangeWithLocation:CPTDecimalFromFloat( -5 ) length:CPTDecimalFromFloat( 10 )]];
     [plotSpace setXRange: [CPTPlotRange plotRangeWithLocation:CPTDecimalFromFloat( 0 ) length:CPTDecimalFromFloat( 20 )]];
     
     // Create the plot (we do not define actual x/y values yet, these will be supplied by the datasource...)
     self.plot = [[CPTScatterPlot alloc] initWithFrame:CGRectZero];
     
     // Set datasource
-    plot.dataSource = [(RAMFFirstViewController *)[self presentingViewController] getModel];
+    self.plot.dataSource = [(RAMFFirstViewController *)[self presentingViewController] getModel];
+    [[(RAMFFirstViewController *)[self presentingViewController] getModel] setDelegate:self];
+
     
     
     // Finally, add the created plot to the default plot space of the CPTGraph object we created before
-    [graph addPlot:plot toPlotSpace:graph.defaultPlotSpace];
-    
-    //[graph setBackgroundColor:[[UIColor redColor] CGColor]];
- 
+    [graph addPlot:self.plot toPlotSpace:graph.defaultPlotSpace];
 }
 
 - (void)viewDidAppear:(BOOL)animated
